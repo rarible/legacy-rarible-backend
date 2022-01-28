3eb54806bfebd8ccdefeb07cc4ceb6ea3a21a0ef
Max <maxime.levillain@functori.com>
2022-01-28 09:54:32 +0100

    BINS: accumulate ext storage diff for recrawl_nft

--- src/tools/recrawl_nft.ml
+++ src/tools/recrawl_nft.ml
@@ -16,30 +16,39 @@ let spec = [
   "--end", Arg.Int (fun i -> recrawl_end := Some (Int32.of_int i)), "Optional end level for recrawl";
 ]
 
-let operation config () op =
+let operation config ext_diff op =
   Format.printf "Block %s (%ld)\r@?" (Common.Utils.short op.bo_block) op.bo_level;
   match op.bo_meta with
   | None -> Lwt.return_error @@
     `generic ("no_metadata", Format.sprintf "no metadata found for operation %s" op.bo_hash)
   | Some meta ->
     if meta.op_status = `applied then
+      let ext_diff = if op.bo_index = 0l then meta.op_lazy_storage_diff else ext_diff in
+      let op_lazy_storage_diff =
+        if op.bo_index = 0l then meta.op_lazy_storage_diff
+        else ext_diff @ meta.op_lazy_storage_diff in
+      let meta = {meta with op_lazy_storage_diff} in
+      let op = { op with bo_meta = Some meta } in
       match op.bo_op.kind with
       | Transaction tr ->
         begin match tr.parameters, SMap.find_opt tr.destination config.Crawlori.Config.extra.Rtypes.contracts with
           | Some {entrypoint; value = Micheline m }, Some nft ->
+            let nft = { nft with Rtypes.nft_crawled = Some true } in
             Db.Misc.use None @@ fun dbh ->
             Format.printf "Block %s (%ld)@." (Common.Utils.short op.bo_block) op.bo_level;
-            Db.Crawl.insert_nft ~dbh ~config ~meta ~op ~contract:tr.destination ~forward:true ~entrypoint ~nft m
-          | _ -> Lwt.return_ok ()
+            let|>? () = Db.Crawl.insert_nft ~dbh ~config ~meta ~op ~contract:tr.destination ~forward:true ~entrypoint ~nft m in
+            ext_diff
+          | _ -> Lwt.return_ok ext_diff
         end
       | Origination ori ->
         let contract = Tzfunc.Crypto.op_to_KT1 op.bo_hash in
         if List.mem contract !contracts then
           Db.Misc.use None @@ fun dbh ->
-          Db.Crawl.insert_origination ~forward:true ~crawled:false config dbh op ori
-        else Lwt.return_ok ()
-      | _ -> Lwt.return_ok ()
-    else Lwt.return_ok ()
+          let|>? () = Db.Crawl.insert_origination ~forward:true ~crawled:false config dbh op ori in
+          ext_diff
+        else Lwt.return_ok ext_diff
+      | _ -> Lwt.return_ok ext_diff
+    else Lwt.return_ok ext_diff
 
 let main () =
   Arg.parse spec (fun f -> filename := Some f) "recrawl_nft.exe [options] config.json";
@@ -49,7 +58,7 @@ let main () =
   config.Cconfig.extra.Rtypes.contracts <- nft_contracts;
   match !recrawl_start with
   | Some start ->
-    let>? _ = async_recrawl ~config ~start ?end_:!recrawl_end ~operation ((), ()) in
+    let>? _ = async_recrawl ~config ~start ?end_:!recrawl_end ~operation ((), []) in
     iter_rp (fun contract -> Db.Crawl.set_crawled_nft contract) !contracts
   | _ ->
     Format.printf "Missing arguments: '--start', '--contracts' are required@.";
