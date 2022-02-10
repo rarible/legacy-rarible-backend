6cdb0f08857d9e144f11ad5d71cb34393a1e9508
Max <maxime.levillain@functori.com>
2022-02-10 17:28:41 +0100

    CRAWLER: wrap empty attributes

--- src/common/parameters.ml
+++ src/common/parameters.ml
@@ -63,8 +63,10 @@ let parse_attributes (l : micheline list) : Json_repr.ezjsonm option =
 let get_string_bytes ?key (h : hex) : Json_repr.ezjsonm option =
   let b = Tzfunc.Crypto.hex_to_raw h in
   try match Tzfunc.Read.(unpack ~typ:(prim `string) b), key with
+    | Ok (Mstring ""), Some "attributes" -> Some (`O []) (* dogami attributes *)
     | Ok (Mstring s), _ -> (* packed string *)
       if decode s then Some (`String s) else None
+
     | Ok (Mseq l), Some "attributes" -> (* dogami attributes *)
       parse_attributes l
     | _ ->
