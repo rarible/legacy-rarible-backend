ba4efd5cb802ca302f55a07ad0fff26ec858eadc
Mika <michael.laporte@functori.com>
2022-04-07 14:01:17 +0200

    tools: add kafka event in contract metadata daemon

--- src/tools/collection_daemon.ml
+++ src/tools/collection_daemon.ml
@@ -3,6 +3,8 @@ open Let
 let node = ref "https://tz.functori.com"
 let retrieve = ref false
 let name = ref false
+let kafka_config_file = ref ""
+let contract = ref ""
 
 let spec = [
   "--node", Arg.Set_string node,
@@ -10,7 +12,9 @@ let spec = [
   "--retrieve", Arg.Set retrieve,
   "Use the context to look for ipfs link";
   "--name-symbol", Arg.Set name,
-  "Retrieve name and symbol fields from metadata bigmap"
+  "Retrieve name and symbol fields from metadata bigmap";
+  "--contract", Arg.Set_string contract, "Retrieve metadata for a contract";
+  "--kafka-config", Arg.Set_string kafka_config_file, "set kafka configuration"
 ]
 
 let usage =
@@ -20,6 +24,7 @@ let () =
   Arg.parse spec (fun _ -> ()) usage;
   EzCurl_common.set_timeout (Some 3);
   Lwt_main.run @@ Lwt.map (Result.iter_error Crp.print_error) @@
+  let>? () = Db.Rarible_kafka.may_set_kafka_config !kafka_config_file in
   if !name then
     let>? l = Db.Utils.contract_metadata_no_name () in
     iter_rp (fun r ->
@@ -40,12 +45,14 @@ let () =
               ~contract:r#address ~block:r#block
               ~level:r#level ~tsp:r#tsp ~metadata:r#metadata n in
         match symbol with
-          | None -> Lwt.return_ok ()
-          | Some s ->
-            Db.Utils.update_contract_metadata_symbol
-              ~contract:r#address ~metadata:r#metadata s) l
+        | None -> Lwt.return_ok ()
+        | Some s ->
+          Db.Utils.update_contract_metadata_symbol
+            ~contract:r#address ~metadata:r#metadata s) l
   else
-    let>? l = Db.Utils.contract_metadata ~retrieve:!retrieve () in
+    let>? l =
+      if !contract <> "" then Db.Utils.metadata !contract
+      else Db.Utils.contract_metadata ~retrieve:!retrieve () in
     iter_rp (fun r ->
         let>? metadata_uri =
           if not !retrieve then
